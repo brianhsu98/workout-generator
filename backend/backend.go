c4e19290cffa5b6a053093c1e28681298ac42fec
Brian Hsu <brian.hsu@databricks.com>
2023-08-05 17:59:45 -0700

    Add in-memory DB and wire it up to service

--- backend/backend.go
+++ backend/backend.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	badger "github.com/dgraph-io/badger/v3"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
-func generateWorkout(c *gin.Context) {
+type Service struct {
+	db *badger.DB
+}
+
+func NewService(db *badger.DB) *Service {
+	return &Service{db: db}
+}
+
+func (app *Service) generateWorkout(c *gin.Context) {
 	prompt := c.PostForm("prompt")
 	fmt.Println(prompt)
+
 	// TODO: Implement calling openAI API
 }
 
 func main() {
+	db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	if err != nil {
+		log.Fatal(err)
+	}
+	service := NewService(db)
+
 	r := gin.Default()
-	r.POST("/prompt", generateWorkout)
+	r.POST("/prompt", service.generateWorkout)
 	r.Run()
 }
